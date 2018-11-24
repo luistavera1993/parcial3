d8fd7f4b4e268c027e1d6127b2219ffecf46b9fe
luistavera1993 <luis.tavera.1014@gmail.com>
2018-11-23 23:14:48 -0400

    biblioteca
    profesor acabo de enviar el proyecto 3 y acabo de notar, que no habia
    cargado como tal los archivos del parcial 3. Los adjunto de nuevo por si
    sirve de algo :(

---
+++ biblioteca/database/migrations/2018_11_23_095710_create_libros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('resumen');
+            $table->string('npagina');
+            $table->string('edicion');
+            $table->string('autor');
+            $table->string('precio',5,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
